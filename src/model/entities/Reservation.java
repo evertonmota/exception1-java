0f22cc8d506ba90d9b93ab6d60c06ccea4b3e563
everton mota <everton.costa_hartb@prestador.globo>
2021-12-07 12:28:33 -0300

    Good Solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exceptions.DomainException;
+
 public class Reservation {
 
 	private Integer roomNumber;
@@ -40,11 +42,22 @@ public class Reservation {
 
 	
 	public long duration() {
+		
 		long diff = checkOut.getTime() - checkIn.getTime();
 		return TimeUnit.DAYS.convert(diff,TimeUnit.MILLISECONDS);
 	}
 
-	public void updateDates(Date checkIn , Date chekOut) {
+	public void updateDates(Date checkIn , Date checkOut) throws DomainException {
+		
+		Date now = new Date();
+		
+		if(checkIn.before(now) || checkOut.before(now)) {
+			throw new DomainException("Reservation dates for update must be future dates ");
+		}
+		if(!checkOut.after(checkIn)) {
+			throw new DomainException("Check-out date must be after check-in date" );
+		}
+
 		this.checkIn  = checkIn;
 		this.checkOut = checkOut;
 	}
---
+++ src/model/exceptions/DomainException.java
@@ -0,0 +1,18 @@
+package model.exceptions;
+
+public class DomainException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public DomainException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public DomainException(String msg) {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+}
